67336ad5d1ba840b3284af4872af8f90ef2630b0
Peter van der Wal <dev@petervanderwal.nl>
2023-12-15 08:07:24 +0100

    puzzle: day 15.2

--- src/Puzzle/Day15.php
+++ src/Puzzle/Day15.php
@@ -19,7 +19,28 @@ class Day15 extends AbstractPuzzle
 
     protected function doCalculateAssignment2(PuzzleInput $input): int|string
     {
-        // TODO: Implement calculateAssignment2() method.
+        $boxes = array_fill(0, 256, []);
+        foreach ($input->split(',') as $string) {
+            $string = (string)$string;
+            if (str_ends_with($string, '-')) {
+                $label = substr($string, 0, -1);
+                $box = $this->hash($label);
+                unset($boxes[$box][$label]);
+            } else {
+                $lens = substr($string, -1);
+                $label = substr($string, 0, -2);
+                $box = $this->hash($label);
+                $boxes[$box][$label] = $lens;
+            }
+        }
+
+        $result = 0;
+        foreach ($boxes as $boxNr => $boxContent) {
+            foreach (array_values($boxContent) as $lensPosition => $lensFocalPoint) {
+                $result += ($boxNr + 1) * ($lensPosition + 1) * $lensFocalPoint;
+            }
+        }
+        return $result;
     }
 
     private function hash(string $string): int
--- tests/Puzzle/Day15Test.php
+++ tests/Puzzle/Day15Test.php
@@ -11,4 +11,7 @@ class Day15Test extends AbstractPuzzleTest
 {
     protected int|string|array $expectedDemo1Value = 1320;
     protected int|string|null $expectedAnswer1Value = 507291;
+
+    protected int|string|array $expectedDemo2Value = 145;
+    protected int|string|null $expectedAnswer2Value = 296921;
 }
